b15f07d5512632a1deb1b579ac40e22593fb0bb1
srsrinath85 <161940964+srsrinath85@users.noreply.github.com>
2025-08-28 16:40:40 +0530

    Add files via upload

---
+++ myjavaapp/src/main/java/com/application/fapplication.java
@@ -0,0 +1,19 @@
+package com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class fapplication {
+    public static void main(String[] args) {
+        SpringApplication.run(fapplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "🚀 Welcome to Fargate automatically update web application.!";
+    }
+}
